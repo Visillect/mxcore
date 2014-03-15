0d585736b93d6a90f0b22b621ecae989e1901975
Anton Grigoryev <me@ansgri.com>
2014-03-15 17:16:00 +0400

    FIX: mxprops/io: license

--- mxprops/io.h
+++ mxprops/io.h
@@ -1,25 +1,54 @@
+/*
+Copyright (c) 2013, Anton Grigoryev. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDERS "AS IS" AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+SHALL COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of copyright holders.
+*/
+
+
 #pragma once
 #include <json-cpp/forwards.h>
 #include "mxprops.h"
-
 
 
 namespace mxprops {
 
 MXPROPS_API
 bool load_from_command_line(mxprops::PTree::Ref const& dst,
-                            std::vector<std::string> & messages, 
-                            int argc, 
+                            std::vector<std::string> & messages,
+                            int argc,
                             char const* argv[]);
 
 MXPROPS_API
 bool load_from_json(mxprops::PTree::Ref const& dst,
-                    std::vector<std::string> & messages, 
+                    std::vector<std::string> & messages,
                     Json::Value const& doc);
 
 MXPROPS_API
 bool load_from_json_file(mxprops::PTree::Ref const& dst,
-                         std::vector<std::string> & messages, 
+                         std::vector<std::string> & messages,
                          std::string const& filename);
 
 }
