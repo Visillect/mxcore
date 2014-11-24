5478bdc51bea21960b6027af2ad15c1835314680
Anton Grigoryev <me@ansgri.com>
2014-11-25 02:23:31 +0300

    FIX: copyright goes to visillect

--- mxprops/pathprop.h
+++ mxprops/pathprop.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2013, Anton Grigoryev. All rights reserved.
+Copyright (c) Visillect Service LLC. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
